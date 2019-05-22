4bf4715b66521e6b1644fcd52594961b17444b99
Rayon Hunte <rayon.hunte@gmail.com>
2019-05-21 22:20:28 -0400

    added menue

--- commando.sh
+++ commando.sh
@@ -6,4 +6,20 @@
 #ng serve
 #cd ..
 
-echo $'\e[1;33m'Welcome to linux hint$'\e[0m'
+echo $'\e[1;33m' Welcome to Commando !!!$'\e[0m'
+echo $'\e[1;33m' Please Chose from below menu Options $'\e[0m'
+
+commands="
+Install-npm-packages 
+Start-CashOut 
+Stop-Cashout
+Restart-Cashout
+Download-Updates
+Show-status
+Genrate-Report
+"
+
+select option in $commands; 
+do
+    echo "Option Chosen:- $REPLY $option"
+done
