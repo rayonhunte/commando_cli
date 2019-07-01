6488581e8c545e8c96593f871cb552e17a267041
rayon hunte <rhunte@localhost.localdomain>
2019-07-01 15:27:58 -0500

    scripts

--- sysd.sh
+++ sysd.sh
@@ -7,36 +7,36 @@ ogPath=$(pwd)
 isSystemd=$(ls /etc/systemd/system/ | grep 'nodeserver' -c)
 
 #node file
-nodeFile=nodeserver.service
+nodeFile=/etc/systemd/system/nodeserver.service
 
 echo $isSystemd
 
 if [ $isSystemd  -ne 1 ]
 then 
-    touch $nodeFile
-    echo "[Unit]" > $nodeFile 
-    echo "Description=Node.js Express server" >>  $nodeFile
-    echo "After=network.target" >> $nodeFile
-    echo "" >> $nodeFile
-    echo "[Service]" >> $nodeFile
-    echo "Enviroment=NODE_PORT=3001" >> $nodeFile
-    echo "Type=simple" >> $nodeFile
-    echo "User=root" >> $nodeFile
-    echo "ExecStart=/usr/bin/node $ogPath/server/server.js" >> $nodeFile
-    echo "Restart=on-failure" >> $nodeFile
-    echo "" >> $nodeFile
-    echo "RestartSec=10" >> $nodeFile
-    echo "" >> $nodeFile
-    echo "[Install]" >> $nodeFile
-    echo "WantedBy=multi-user.target" >> $nodeFile
+    sudo touch $nodeFile
+    sudo sh -c "echo [Unit] > $nodeFile" 
+    sudo sh -c "echo Description=Node.js Express server >>  $nodeFile"
+    sudo sh -c "echo After=network.target >> $nodeFile"
+    sudo sh -c "echo '' >> $nodeFile"
+    sudo sh -c "echo [Service] >> $nodeFile"
+    sudo sh -c "echo Enviroment=NODE_PORT=3001 >> $nodeFile"
+    sudo sh -c "echo Type=simple >> $nodeFile"
+    sudo sh -c "echo User=root >> $nodeFile"
+    sudo sh -c "echo ExecStart=/usr/bin/node $ogPath/server/server.js >> $nodeFile"
+    sudo sh -c "echo Restart=on-failure >> $nodeFile"
+    sudo sh -c "echo '' >> $nodeFile"
+    sudo sh -c "echo RestartSec=10 >> $nodeFile"
+    sudo sh -c "echo '' >> $nodeFile"
+    sudo sh -c "echo [Install] >> $nodeFile"
+    sudo sh -c "echo WantedBy=multi-user.target >> $nodeFile"
     #sudo chmod a=rwx $nodeFile
     #sudo chown root $nodeFile
     #sudo chgrp root $nodeFile
-    sudo mv $nodeFile isSystemd
+    #sudo mv $nodeFile /etc/systemd/system/
     sudo systemctl daemon-reload
 
 fi
 
 #start app no matter what
-#sudo systemctl start nodeserver
+sudo systemctl start nodeserver
 
